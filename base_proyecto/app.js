a0446a103cbdecd0997337f5adbd344c75376442
manutj <manuel.tejada95@outlook.com>
2024-11-05 21:05:32 -0600

    creando cards de pokemones con axios y DOM

--- base_proyecto/app.js
+++ base_proyecto/app.js
@@ -1,16 +1,44 @@
-let allPokemons = []
 
 const getPokemons = (limit) => {
+let allPokemons = []
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}` 
 axios.get(url)
 .then((response)=>{
     let infoPokemons = response.data.results
-    console.log(infoPokemons)
+    //console.log(infoPokemons)
+    infoPokemons.forEach((pokemon)=>{
+       axios.get(pokemon.url) 
+       .then((moreInfoPokemon)=>{
+       //console.log(moreInfoPokemon.data)
+
+        let infoIndividualPokemon = {
+            nombre: pokemon.name,
+            tipos: moreInfoPokemon.data.types.map((tipo)=>tipo.type.name),
+            urlImage : moreInfoPokemon.data.sprites.front_default
+        }
+
+        //console.log(infoIndividualPokemon)
+
+        allPokemons.push(infoIndividualPokemon)
+       })
+       .catch((error)=>console.log(error))
+    })
+    //console.log(allPokemons)
 })
+.catch((error)=>console.log(error))
+
+// VER COMO IMPLEMENTAR PROMESAS EN LUGAR DE TIMEOUTS
+
+setTimeout(() => {
+    createCards(allPokemons)
+}, 3000);
+
+
 }
 
 
 let createCards = (pokemons) => {
+    console.log('Ejecutando funcion', pokemons.length)
     let pokemonsContainer = document.getElementById('pokemon-container')
     pokemons.forEach((pokemon)=>{
         let card = document.createElement('div')
@@ -20,5 +48,11 @@ let createCards = (pokemons) => {
         card.append(title, image);
         pokemonsContainer.append(card)
 
+        title.innerText = pokemon.nombre
+        image.src = pokemon.urlImage
+
+        card.setAttribute('class', 'card')
+
     })
 }
+
